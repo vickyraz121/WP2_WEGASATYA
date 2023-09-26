144169627521febb246d0b3b0f80d01fc6e06d78
vickyraz121 <vickyraz121@gmail.com>
2023-09-26 19:01:12 +0700

    tugas1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Wega Satya
+        Saya tinggal di daerah jakarta timur
+        olahraga yang saya sukai adalah berenang";
+    }
+}
